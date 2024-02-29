e00506548abfac3e958e60e620e6460182cca331
snallamada <shravanreddynallamada@gmail.com>
2024-02-29 16:58:50 +0100

    update code

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,14 @@
+const Name = "nsr"
+let age  ;
+age = 45;
+console.log(Name, age)
+
+// if and else comparision test
+const birthYear = 2012;
+let century;
+if(birthYear <= 2000){
+century = "20th century";
+}else{
+    century = "21th century";
+}
+console.log(century)
